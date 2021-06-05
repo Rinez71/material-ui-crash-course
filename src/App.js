e76210d790016bfb548d6844f6c98094c1d86d5c
Robert Inez <rinez@cnm.edu>
2021-06-05 12:23:10 -0600

    starting work on materialui crash course

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+	Typography,
+	AppBar,
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
+	CssBaseline,
+	Grid,
+	Toolbar,
+	Container,
+	Drawer,
+	Button
+} from '@material-ui/core';
+import {Business} from '@material-ui/icons';
+import useStyles from './styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const classes = useStyles();
+	return (
+		<>
+			<CssBaseline />
+			<AppBar position="relative">
+				<Toolbar>
+					<Business className={classes.icon}/>
+					<Typography variant='h6'>
+						Business Name
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<main>
+				<div className={classes.container}>
+					<Container maxWidth='sm'>
+						<Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+							Business Name
+						</Typography>
+						<Typography variant='h5' align='center' color='textSecondary' paragraph>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci aperiam
+							   aut, blanditiis consequatur culpa, cumque cupiditate earum facilis illo itaque laudantium
+							   molestiae neque nihil nobis quia quidem reprehenderit sequi.
+						</Typography>
+						<div className={classes.button}>
+							<Grid container spacing={2} justify="center">
+								<Grid item>
+									<Button variant='contained' color='primary'>
+										Call to Action
+									</Button>
+								</Grid>
+								<Grid item>
+									<Button variant='outlined' color='primary'>
+										Learn More
+									</Button>
+								</Grid>
+							</Grid>
+						</div>
+					</Container>
+				</div>
+				<Container className={classes.cardGrid} maxWidth='md'>
+					<Grid container spacing={4}>
+						<Grid>
+
+						</Grid>
+					</Grid>
+				</Container>
+			</main>
+		</>
+	);
+};
 
 export default App;
